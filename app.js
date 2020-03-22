55a880169eba2da888cf805fd83a6193bc69a7bf
David Seek <davidseek1986@gmail.com>
2020-03-22 15:14:17 -0700

    added video

---
+++ app.js
@@ -0,0 +1,9 @@
+navigator.mediaDevices
+    .getUserMedia({
+        video: true
+    })
+    .then(function (stream) {
+        document.getElementById('camera').srcObject = stream;
+    }).catch(function () {
+        alert('could not connect stream');
+    });
